9c4fd09608bda8dce3f773a784ac5469f657de8a
Jared Wasinger <j-wasinger@hotmail.com>
2014-08-24 22:14:22 -0400

    .

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
-var WebSocketServer = require('ws').Server;
-var wss = new WebSocketServer({port: 8000});
+//var WebSocketServer = require('ws').Server;
+//var wss = new WebSocketServer({port: 8000});
 
-wss.on('connection', function(ws)
-{
-	ws.on('message', function(message)
-	{
-		console.log('received %s', message);
-	});
-	ws.send('something');
+//wss.on('connection', function(ws)
+//{
+//	ws.on('message', function(message)
+//	{
+//		console.log('received %s', message);
+//	});
+//	ws.send('something');
+//});
+
+var http = require("http");
+var server = http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("<!DOCTYPE 'html'>");
+    response.write("<html>");
+    response.write("<head>");
+    response.write("<title>Hello World Page</title>");
+    response.write("</head>");
+    response.write("<body>");
+    response.write("Hello World!");
+    response.write("</body>");
+    response.write("</html>");
+    response.end();
 });
+ 
+ server.listen(80);
